7f2d805ad30beef0e5ec2ebb7ea79cd47252cf45
pario <pario@yeahka.com>
2020-05-26 16:15:39 +0800

    init

---
+++ src/main/java/com/pkc/mpring/framework/LoginInterceptor.java
@@ -0,0 +1,11 @@
+package com.pkc.mpring.framework;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+/**
+ * @author Pario
+ * @description 登录拦截器
+ * @date 2020/5/26 15:45
+ */
+public class LoginInterceptor extends HandlerInterceptorAdapter {
+}
